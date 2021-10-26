80495c8025b9e2e55365203ff4f076757fe244bb
PALVADEAU  Florian <florian.palvadeau@edu.devinci.fr>
2021-10-26 14:01:31 +0200

    :art::bug: adding JS scroll animation and fixing bugs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const navbar = document.querySelector(".navbar");
 
 menuicon.addEventListener("click", () => {
     navbar.classList.toggle("change");
-    console.log('ez')
 });
     
     
@@ -93,8 +92,151 @@ function topFunction() {
         left: 0,
         behavior: "smooth",
     })
-}
+};
 
 
 
 
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+  var revealLeft = document.querySelectorAll('.revealLeft');
+  var revealRight = document.querySelectorAll('.revealRight');
+  var reveal2 = document.querySelectorAll('.reveal2');
+  var reveal3 = document.querySelectorAll('.reveal3');
+  var reveal4 = document.querySelectorAll('.reveal4');
+  var revealimg = document.querySelectorAll('.revealimg');
+  var revealimgLeft = document.querySelectorAll('.revealimgLeft');
+  var revealimgRight = document.querySelectorAll('.revealimgRight');
+  var revealimg2 = document.querySelectorAll('.revealimg2');
+
+
+    for (var i = 0 ; i < reveals.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = reveals[i].getBoundingClientRect().top;
+      var revealPoint = 150;
+
+      if (revealTop < windowHeight - revealPoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0 ; i < revealLeft.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = revealLeft[i].getBoundingClientRect().top;
+      var revealPoint = 200;
+
+      if (revealTop < windowHeight - revealPoint){
+        revealLeft[i].classList.add('active');
+      }
+      else{
+        revealLeft[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0 ; i < revealRight.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = revealRight[i].getBoundingClientRect().top;
+      var revealPoint = 200;
+
+      if (revealTop < windowHeight - revealPoint){
+        revealRight[i].classList.add('active');
+      }
+      else{
+        revealRight[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0 ; i < reveal2.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = reveal2[i].getBoundingClientRect().top;
+      var revealPoint = 130;
+
+      if (revealTop < windowHeight - revealPoint){
+        reveal2[i].classList.add('active');
+      }
+      else{
+        reveal2[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0 ; i < reveal3.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = reveal3[i].getBoundingClientRect().top;
+      var revealPoint = 150;
+
+      if (revealTop < windowHeight - revealPoint){
+        reveal3[i].classList.add('active');
+      }
+      else{
+        reveal3[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0 ; i < reveal4.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = reveal4[i].getBoundingClientRect().top;
+      var revealPoint = 180;
+
+      if (revealTop < windowHeight - revealPoint){
+        reveal4[i].classList.add('active');
+      }
+      else{
+        reveal4[i].classList.remove('active');
+      }
+    }
+
+    for (var i = 0 ; i < revealimg.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = revealimg[i].getBoundingClientRect().top;
+      var revealPoint = 250;
+
+      if (revealTop < windowHeight - revealPoint){
+        revealimg[i].classList.add('active');
+      }
+    }
+
+    for (var i = 0 ; i < revealimg2.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = revealimg2[i].getBoundingClientRect().top;
+      var revealPoint = 270;
+
+      if (revealTop < windowHeight - revealPoint){
+        revealimg2[i].classList.add('active');
+      }
+    }
+
+    for (var i = 0 ; i < revealimgLeft.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = revealimgLeft[i].getBoundingClientRect().top;
+      var revealPoint = 250;
+
+      if (revealTop < windowHeight - revealPoint){
+        revealimgLeft[i].classList.add('active');
+      }
+    }
+
+    for (var i = 0 ; i < revealimgRight.length ; i++){
+
+      var windowHeight = window.innerHeight;
+      var revealTop = revealimgRight[i].getBoundingClientRect().top;
+      var revealPoint = 250;
+
+      if (revealTop < windowHeight - revealPoint){
+        revealimgRight[i].classList.add('active');
+      }
+    }
+}  
