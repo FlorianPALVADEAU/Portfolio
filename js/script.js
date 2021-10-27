e910bb5657d81b76d165a47176e045311b5cd881
PALVADEAU  Florian <florian.palvadeau@edu.devinci.fr>
2021-10-27 12:23:54 +0200

    :art: :lipstick: finishing last details

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
+
+//PAGE LOADER
 $(window).on("load", function () {
     $(".loader-wrapper").fadeOut("slow");
 });
 
+
+//MENU BURGER
 const menuicon = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector(".navbar");
 
@@ -10,74 +14,75 @@ menuicon.addEventListener("click", () => {
 });
     
     
+//SLIDER
+new Glide('.glide').mount();
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZmxvcGx2ZCIsImEiOiJja3Y0eWN0ZTgxMTBmMnZzMzk5b3piNHI1In0.oT3Wlk6SQ7B-mKHV61VMGQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/floplvd/ckv4xv4822tp415qmf7tm3wpj',
+    center: [35, 47],
+    speed: 0.6,
+    zoom: 3 
+});
+
+//MAPBOX
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [1.900330766926106, 48.80262283112147]
+        },
+        properties: {
+          title: 'Jouars-Pontchartrain',
+          description: 'Here is the city where i live !'
+        }
+      }
+    ]
+  };
+
+  geojson.features.forEach(function (marker) {
+
+    var el = document.createElement('div');
+    el.className = 'marker';
     
-    new Glide('.glide').mount();
-
-
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZmxvcGx2ZCIsImEiOiJja3Y0eWN0ZTgxMTBmMnZzMzk5b3piNHI1In0.oT3Wlk6SQ7B-mKHV61VMGQ';
-    const map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/floplvd/ckv4xv4822tp415qmf7tm3wpj',
-        center: [35, 47],
-        speed: 0.6,
-        zoom: 3 
-    });
-
-
-    const geojson = {
-        type: 'FeatureCollection',
-        features: [
-          {
-            type: 'Feature',
-            geometry: {
-              type: 'Point',
-              coordinates: [1.900330766926106, 48.80262283112147]
-            },
-            properties: {
-              title: 'Jouars-Pontchartrain',
-              description: 'Here is the city where i live !'
-            }
-          }
-        ]
-      };
-
-      geojson.features.forEach(function (marker) {
-
-        var el = document.createElement('div');
-        el.className = 'marker';
+    let tmpMarker = new mapboxgl.Marker(el)
+    let tmpPopUp = new mapboxgl.Popup()
+        .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>')
     
-        let tmpMarker = new mapboxgl.Marker(el)
-        let tmpPopUp = new mapboxgl.Popup()
-            .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>')
+    el.markerInstance = tmpMarker;
     
-        el.markerInstance = tmpMarker;
+    tmpMarker
+        .setLngLat(marker.geometry.coordinates)
+        .setPopup(tmpPopUp)
+        .addTo(map);
     
-        tmpMarker
-            .setLngLat(marker.geometry.coordinates)
-            .setPopup(tmpPopUp)
-            .addTo(map);
-    
-        el.addEventListener("click", e => {
-            let coords = e.target.markerInstance.getLngLat();
-            map.flyTo({
-                bearing: 10,
-                pitch: 50,
-                center: [1.900330766926106, 48.80262283112147],
-                zoom: 16,
-                speed: 0.8,
-            });
+    el.addEventListener("click", e => {
+        var pMap = document.querySelector('.pMap')
+        let coords = e.target.markerInstance.getLngLat();
+        pMap.classList.add('noDisplay')
+        map.flyTo({
+            bearing: 10,
+            pitch: 50,
+            center: [1.900330766926106, 48.80262283112147],
+            zoom: 16,
+            speed: 0.8,
         });
-        
     });
+    
+});
 
 
 
-
+//SCROLLTOP BUTTON
 mybutton = document.querySelector(".boutonScrollTop");
 mybutton.classList.add('noDisplay');
 
 window.onscroll = function() {scrollFunction()};
-
 
 function scrollFunction() {
   if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
@@ -96,7 +101,7 @@ function topFunction() {
 
 
 
-
+//REVEAL ON SCROLL
 window.addEventListener('scroll', reveal);
 
 function reveal(){
